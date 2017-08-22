deb02d813799d0e2e603ad73b9ee617192b7af26
Don Yash <donyash@hotmail.com>
2017-08-22 11:26:33 -0700

    added constants

--- src/actions/courseActions.js
+++ src/actions/courseActions.js
@@ -1,4 +1,5 @@
+import * as types from './actionTypes';
 export function createCourse(course){
     //debugger;
-    return{ type: 'CREATE_COURSE', course: course};
+    return{ type: types.CREATE_COURSE, course: course};
 }
--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -1,6 +1,7 @@
 import React, {PropTypes} from 'react';
 import {connect} from 'react-redux';
 import * as courseActions from '../../actions/courseActions';
+import {bindActionCreators} from 'redux';
 
 class CoursesPage extends React.Component {
     constructor(props, context) {
@@ -22,7 +23,7 @@ class CoursesPage extends React.Component {
 
     onClickSave(){
         //alert(`Saving ${this.state.course.title}` );
-        this.props.dispatch(courseActions.createCourse(this.state.course));
+        this.props.actions.createCourse(this.state.course);
     }
 
     courseRow(course, index){
@@ -47,7 +48,7 @@ class CoursesPage extends React.Component {
    }
 }
 CoursesPage.propTypes = {
-    dispatch: PropTypes.func.isRequired,
+    actions: PropTypes.object.isRequired,
     courses: PropTypes.array.isRequired
 };
 function mapStateToProps(state, ownProps){
@@ -57,5 +58,10 @@ function mapStateToProps(state, ownProps){
     };
 }
 
-
-export default connect(mapStateToProps) (CoursesPage);
+function mapDispatchToProps(dispatch){
+    return{
+        //createCourse: course => dispatch(courseActions.createCourse(course))
+        actions: bindActionCreators(courseActions, dispatch)
+    };
+}
+export default connect(mapStateToProps, mapDispatchToProps) (CoursesPage);
