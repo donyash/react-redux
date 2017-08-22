647a0a7faa0e9c70c1c850161be599f7b7141744
Don Yash <donyash@hotmail.com>
2017-08-22 08:28:36 -0700

    initial checkin

---
+++ src/components/about/AboutPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class AboutPage extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>About </h1>
+                <p> This app uses React, Redux, React router</p>
+            </div>
+            
+        );
+    }
+}
+
+export default AboutPage;
---
+++ src/components/home/HomePage.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {Link} from 'react-router';
+
+class HomePage extends React.Component{
+    render(){
+
+        return (
+            <div className="jumbotron">
+                <h1>Administration </h1>
+                <p> React, Redux and React Router</p>
+                <Link to="about" className="btn btn-primary btn-lg">Learn more</Link>
+            </div>
+
+    
+        );
+    }
+}
+
+export default HomePage;
