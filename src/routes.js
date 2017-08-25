447cd6178e5b2250e0ffa5881bc6f565d546bd8e
Yash <dpy@gasco.com>
2017-08-25 11:00:45 -0700

    added manage course form

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ import App from './components/App';
 import HomePage from './components/home/HomePage';
 import AboutPage from './components/about/AboutPage';
 import CoursesPage from './components/course/CoursesPage';
+import ManageCoursePage from './components/course/ManageCoursePage';
 
 
 export default(
@@ -11,6 +12,8 @@ export default(
     <Route path="/" component={App}>
         <IndexRoute component={HomePage} />
         <Route path="courses" component={CoursesPage} />
+        <Route path="course" component={ManageCoursePage} />
+        <Route path="course/:id" component={ManageCoursePage} />
         <Route path="about" component={AboutPage} />
     </Route>
 );
