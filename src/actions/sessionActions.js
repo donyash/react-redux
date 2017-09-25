efb8b87cf90e51af53d67a4f4dba5e5e62f47ce2
Don Yash <donyash@hotmail.com>
2017-09-25 15:32:18 -0700

    dispatch for logout success

--- src/actions/sessionActions.js
+++ src/actions/sessionActions.js
@@ -5,20 +5,36 @@ import {browserHistory} from 'react-router';
 import toastr from 'toastr';
 
 export function loginSuccess() {  
-  return {type: types.LOG_IN_SUCCESS};
+  return {type: types.LOG_IN_SUCCESS
+    ,logged_in: true,  profile: {user: sessionStorage.getItem('user'), token: sessionStorage.getItem('jwt')}};
 }
+
 export function loginFailure() {  
   return {type: types.LOG_IN_FAILURE};
 }
 
-export function logOutUser() {  
-  sessionStorage.removeItem('jwt');
-  sessionStorage.removeItem('user');
-  
-  browserHistory.push('/');
-  toastr.info('Log out successful');
-  return {type: types.LOG_OUT};
+export function receiveLogout() {
+  return {
+    type: types.LOG_OUT
+    ,logged_in: false,  profile: {user: "", token: null}
+  };
 }
+
+export function logOutUser() {  
+  return dispatch => {
+    //dispatch(requestLogout())
+    sessionStorage.removeItem('jwt');
+    sessionStorage.removeItem('user');
+    toastr.info('Log out successful');
+    dispatch(receiveLogout());
+  };
+}
+  // sessionStorage.removeItem('jwt');
+  // sessionStorage.removeItem('user');
+  
+  // browserHistory.push('/');
+  // toastr.info('Log out successful');
+  // return {type: types.LOG_OUT};
 
 export function logInUser(credentials) {  
   return function(dispatch) {
